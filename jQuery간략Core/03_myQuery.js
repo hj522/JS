1036035a394b37b9444b27371e192b533cda56b1
hj522 <jjungvvv@naver.com>
2022-09-08 20:20:42 +0900

    Javascript Study

--- jQuery간략Core/03_myQuery.js
+++ jQuery간략Core/03_myQuery.js
@@ -1,5 +1,5 @@
     (function(){
-        alert("난 부르지 않아도 실행되어용");
+     //   alert("난 부르지 않아도 실행되어용");
 
         var $ = function(p_sel){
             return new myDom(p_sel);
@@ -7,6 +7,9 @@
 
         var myDom = function(p_sel) {
             var v_elems = document.querySelectorAll(p_sel);
+            this.prev = p_sel; // 실제 jQuery는 이렇게 하지 않음
+            // 최초 넘어온 선택자 값을 저장만 해둠
+            // jQuery방식과 지금 방식은 사용 상황에 따라 득과 실이 다름
             this.length = v_elems.length;
             for(var i=0; i<v_elems.length; i++) { 
                 this[i] = v_elems[i];
@@ -19,10 +22,14 @@
 
     // 메소드들
     $.fn.eq = function(p_index) {   
-   //     return this[p_index];
-   // 안 바쁜 사람만 요기를 어떻게 만들면 실제 jQuery처럼 eq처럼
-   // 메소드 체인닝 되게 만들 수 있는지 해보기~~
-        return this;
+   //     return this[p_index];   //요렇게 메소드 체인닝 사용 불가
+   // 선택된 객체 1개만 남기고 this(여기선 myDom)을 리턴해야 함
+        this[0] = this[p_index]; // 선택된 객체를 맨앞으로 옮기고
+        for(var i=1; i<this.length; i++) {  // 나머진 지워버림
+            delete this[i];
+        }
+        this.length = 1; 
+        return this;    // 선택된 객체 1개만 가진 myDom타입 객체 리턴
     }
  
     // innerHTML속성을 컨트롤하는 메소드, 필수 메소드!!
@@ -122,5 +129,33 @@
         }
         return this; // 메소드 체인닝
     }
+
+    // style객체를 다루는 메소드
+    $.fn.css = function(p_arg1, p_arg2) {
+        if(typeof(p_arg1) == "string" && !p_arg2) {     // 읽기
+            return this[0].style[p_arg1];
+        }
+
+        if(typeof(p_arg1) == "string" && typeof(p_arg2) == "string"){   // 쓰기
+            for(var i=0; i<this.length; i++) {
+                this[i].style[p_arg1] = p_arg2;
+            }
+            return this; // 메소드 체인닝
+        }
+
+        if(typeof(p_arg1) == "string" && typeof(p_arg2) == "function"){   // 쓰기
+            for(var i=0; i<this.length; i++) {
+                this[i].style[p_arg1] = p_arg2.call(this[i],i,this[i].style[p_arg1]);
+            }
+            return this; // 메소드 체인닝
+        }
+    }
+
+$.fn.end = function(){
+    // 힌트: 최초의 선택자를 보관한다면? how?
+    return $("this.prev");  // 이미 저장해둔 선택자 값으로 다시 생성
+}
+    // jQuery 메소드의 사용법에 일관된 패턴이 있음을 느꼈으면 좋겠어용
+    // 읽기, 전부 쓰기, 골라서 쓰기
 
     
